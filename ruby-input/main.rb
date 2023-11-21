fffe2f892f664f4502694df93e7f1d5d9dfc76a5
Sami Hindi <lolipop66611@gmail.com>
2023-11-21 08:04:28 +0100

    Add input from user

---
+++ ruby-input/main.rb
@@ -0,0 +1,10 @@
+puts "What is your first name?"
+first_name = gets.chomp
+
+puts "What is your last name?"
+last_name = gets.chomp
+
+puts "How old are you?"
+age = gets.chomp
+
+print("You are #{first_name} #{last_name} and are #{age} years old.")
